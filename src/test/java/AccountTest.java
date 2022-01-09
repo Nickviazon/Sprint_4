ab216277b36e080b2175400f9d2f42cc620e7721
Vitaliy Fundovoy <nickvi@bk.ru>
2022-01-09 20:19:28 +0300

    Cases with first/last space instead of word were added

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -19,13 +19,16 @@ public class AccountTest {
                 {"Karl Third", true}, // строка в диапазоне 3 <= x <= 19 (два слова с 1 пробелом)
                 {"K T", true}, // 3 символа (буква пробел буква)
                 {"KarlThird KarlThird", true}, // 19 символов (2 слова с 1 пробелом)
+                {"KarlThird KarlThirdK", false}, // более 19 сомволов в стороке
                 {"", false}, // пустая строка
                 {" ", false}, // единичный пробел
                 {"KarlThird", false}, // 1 слово без пробелов
                 {"Karl  Third", false}, // два пробела между словами
                 {" Karl Third", false}, // пробел вначале
                 {"Karl Third ", false}, // пробел вконце
-                {"Karl Third Karl", false} // более 2 слов разделенных пробелами
+                {"Karl Third Karl", false}, // более 2 слов разделенных пробелами
+                {"  KarlThird", false}, // пробел вместо первого слова
+                {"KarlThird  ", false}, // пробел вместо второго слова
         };
     }
 
